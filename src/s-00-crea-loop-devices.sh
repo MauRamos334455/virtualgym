ebd9d728a146acddcdce4f4ba379dba184ffb001
MauRamos334455 <mauri334455@gmail.com>
2021-02-03 23:53:42 -0600

    Creacion de loop devices

---
+++ src/s-00-crea-loop-devices.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+# @Autor  Carolina Kenedy Villa
+#         Cesar Mauricio Ramos Villaseñor
+# @Fecha 18/10/2020
+# @Descripción Programa que crea loop
+# devices para la base de datos del proyecto.
+# Ejecutarse como root
+
+# Crear archivos img, se considera que la carpeta /unam-bda ya fue creada
+cd /unam-bda
+dd if=/dev/zero of=disk11.img bs=100M count=10
+dd if=/dev/zero of=disk12.img bs=100M count=10
+dd if=/dev/zero of=disk13.img bs=100M count=10
+dd if=/dev/zero of=disk14.img bs=100M count=10
+
+# Comprobar creación de los archivos
+du -sh disk*.img
+
+# Crear loop devices asociándolos a su archivo
+losetup -fP disk11.img
+losetup -fP disk12.img
+losetup -fP disk13.img
+losetup -fP disk14.img
+
+
+# Confirmamos creación del loop device
+losetup -a
+
+# Dar formato de archivos
+mkfs.ext4 disk11.img
+mkfs.ext4 disk12.img
+mkfs.ext4 disk13.img
+mkfs.ext4 disk14.img
+
+# Creación de los puntos de montaje
+mkdir /u11 /u12 /u13 /u14
+
+# Resta aplicar montaje de manera manual o por medio
+# de archivo fstab
