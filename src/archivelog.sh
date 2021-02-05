319832e647983e179d4e32c001feda9b3ef57365
UstehKenny <carosiim.sdco@gmail.com>
2021-02-05 14:45:37 -0600

    :v

--- src/archivelog.sh
+++ src/archivelog.sh
@@ -1,5 +1,5 @@
-mkdir -p /unam-bda/archivelogs/KERAPROY/disk_a
-mkdir -p /unam-bda/archivelogs/KERAPROY/disk_b
-chown -R oracle:oinstall /unam-bda/archivelogs
-chmod -R 750 /unam-bda/archivelogs
+sudo mkdir -p /unam-bda/archivelogs/KERAPROY/disk_a
+sudo mkdir -p /unam-bda/archivelogs/KERAPROY/disk_b
+sudo chown -R oracle:oinstall /unam-bda/archivelogs
+sudo chmod -R 750 /unam-bda/archivelogs
 
