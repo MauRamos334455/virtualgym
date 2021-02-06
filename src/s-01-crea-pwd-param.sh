7b1231091ce27dd7f72c74b0f93e47ca24fbf979
MauRamos334455 <mauri334455@gmail.com>
2021-02-06 01:28:06 -0600

    creacion script fra

--- src/s-01-crea-pwd-param.sh
+++ src/s-01-crea-pwd-param.sh
@@ -26,6 +26,7 @@ echo -e "db_name=keraproy\n
 control_files=(/u11/app/oracle/oradata/KERAPROY/control01.ctl,
 /u12/app/oracle/oradata/KERAPROY/control02.ctl,
 /u13/app/oracle/oradata/KERAPROY/control03.ctl)\n
+db_domain='fi.unam'\n
 memory_target=768M" >> $ORACLE_HOME/dbs/initkeraproy.ora
 
 echo "---------- Listo! ----------"
--- src/s-09-fra.sql
+++ src/s-09-fra.sql
@@ -3,3 +3,10 @@
 -- @Fecha: 05/02/2021
 -- @DescripciÓn: Configuraciones para la
 -- FRA en la base de datos
+
+connect sys/systemgym as sysdba
+
+create pfile from spfile;
+alter system set db_recovery_file_dest_size=1354m scope=both;
+alter system set db_recovery_file_dest='/unam-bda/fast-reco-area' scope=both;
+alter system set db_flashback_retention_target=1440 scope=both;
