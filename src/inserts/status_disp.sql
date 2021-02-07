37f55d7863327378b26a63036364d7fb4f4b00f1
MauRamos334455 <mauri334455@gmail.com>
2021-02-06 21:35:38 -0600

    Eliminacion de archivos

--- src/inserts/status_disp.sql
+++
@@ -1,5 +0,0 @@
-insert into status_disp (status_id, clave, descripcion) values (296, 'REP', 'Dispositivo en reparación');
-insert into status_disp (status_id, clave, descripcion) values (297, 'VIG', 'Dispositivo vigente');
-insert into status_disp (status_id, clave, descripcion) values (298, 'FUS', 'Dispositivo fuera de uso');
-insert into status_disp (status_id, clave, descripcion) values (299, 'AVE', 'Dispositivo averiado');
-insert into status_disp (status_id, clave, descripcion) values (300, 'OBS', 'Dispositivo obsoleto');
