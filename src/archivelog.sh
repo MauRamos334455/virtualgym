5989c6952ed17efbe3faa205e8278e7c388fa9e8
MauRamos334455 <mauri334455@gmail.com>
2021-02-06 22:10:46 -0600

    Eliminacion archive.sh

--- src/archivelog.sh
+++
@@ -1,5 +0,0 @@
-sudo mkdir -p /unam-bda/archivelogs/KERAPROY/disk_a
-sudo mkdir -p /unam-bda/archivelogs/KERAPROY/disk_b
-sudo chown -R oracle:oinstall /unam-bda/archivelogs
-sudo chmod -R 750 /unam-bda/archivelogs
-
